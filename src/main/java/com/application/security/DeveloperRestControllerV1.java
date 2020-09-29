3a6b0a569800e69a8d85305f240e2d0b6b6fab69
Alexandru Simon <simonsanea@gmail.com>
2020-09-29 11:19:17 +0300

    Implementing Spring Security JWT Token Authentication

---
+++ src/main/java/com/application/security/DeveloperRestControllerV1.java
@@ -0,0 +1,43 @@
+package com.application.security;
+
+import com.application.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+    private List<Developer> developers = Stream.of(
+            new Developer(1, "Alex", "Simon"),
+            new Developer(2, "John", "Rock"))
+            .collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
+    public Developer getById(@PathVariable Integer id) {
+        return developers.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst().orElse(null);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
+    public Developer create(Developer developer) {
+        this.developers.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
+    public void deleteById(@PathVariable Integer id) {
+        this.developers.removeIf(developer -> developer.getId().equals(id));
+    }
+}
