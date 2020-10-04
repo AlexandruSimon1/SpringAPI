506d4d30121390bcd593781fecef55a4b85c2884
Alexandru Simon <simonsanea@gmail.com>
2020-10-04 17:16:35 +0300

    Updated

---
+++ src/main/java/com/application/mapper/TableMapper.java
@@ -0,0 +1,19 @@
+package com.application.mapper;
+
+import com.application.dto.TableDTO;
+import com.application.model.Table;
+import org.mapstruct.Context;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TableMapper {
+    TableMapper INSTANCE = Mappers.getMapper(TableMapper.class);
+
+    TableDTO toTableDto(Table table, @Context NotificatorMappingContext context);
+
+    @InheritInverseConfiguration
+    Table fromTableDto(TableDTO tableDTO, @Context NotificatorMappingContext context);
+}
