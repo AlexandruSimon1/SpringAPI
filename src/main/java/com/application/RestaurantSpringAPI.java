b392caf4fc4b7effd2747953bd4e41be283f27a8
Alexandru Simon <simonsanea@gmail.com>
2020-10-10 09:33:13 +0300

    Created the correct map for controller test

--- src/main/java/com/application/RestaurantSpringAPI.java
+++ src/main/java/com/application/RestaurantSpringAPI.java
@@ -1,18 +1,11 @@
 package com.application;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestaurantSpringAPI {
     public static void main(String[] args) {
         SpringApplication.run(RestaurantSpringAPI.class, args);
-    }
-
-    @Bean
-    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
     }
 }
